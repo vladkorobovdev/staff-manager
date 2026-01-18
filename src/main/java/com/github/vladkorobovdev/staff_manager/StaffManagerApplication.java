ebd93cc8dfb1c3f3d7be78e39558a06881a2eed8
Uladzislau <vladkorobov.work@gmail.com>
2026-01-18 22:10:06 +0100

    add: init files

---
+++ src/main/java/com/github/vladkorobovdev/staff_manager/StaffManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.vladkorobovdev.staff_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/vladkorobovdev/staff_manager/StaffManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vladkorobovdev.staff_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
